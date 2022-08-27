fdbfd61eecb6cc24d822058ad9e55f29eb324d27
Rohit Choudhary <71605903+Rohitchaudhary13@users.noreply.github.com>
2022-08-27 17:32:31 +0530

    Add files via upload

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xc1332D08Ce6aeFE94406dB05593bE1CB094B2617";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sandy-seven.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
